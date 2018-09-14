e23d0e3713255b0ece33f2b9a3837dcd083c571b
Nathan Dench <ndenc2@gmail.com>
2018-09-15 00:18:01 +1000

    feat(blue-green): Create load balancer and code deploy group

--- README.md
+++ README.md
@@ -21,3 +21,23 @@ it access to create and destroy resources:
 ```sh
 $ aws configure
 ```
+
+Make sure the artifact exists in s3:
+
+```sh
+$ cd artifact
+$ zip -r ../artifact.zip *
+$ cd ..
+$ aws s3 cp artifact.zip s3://sunfish-all-general/test/devops-playgound.zip
+```
+
+Create an application revision:
+
+```sh
+$ aws deploy register-application-revision --application-name devops-playground --s3-location bucket=sunfish-all-general,bundleType=zip,key=test/devops-playground.zip --region ap-southeast-1
+```
+
+## TODO:
+
+- Get codedeploy to deploy a revision
+- Remove default nginx html
--- main.tf
+++ main.tf
@@ -122,12 +122,90 @@ module "autoscaling" {
     "${module.security_group_web.this_security_group_id}",
   ]
 
-  health_check_type = "EC2"
   min_size          = 1
   max_size          = 1
   desired_capacity  = 1
 
+  target_group_arns = ["${module.loadbalancer.target_group_arns}"]
+  health_check_type = "ELB"
+  min_elb_capacity  = 1
+
   recreate_asg_when_lc_changes = true
 
   tags_as_map = "${local.default_tags}"
+
+  tags = [
+    {
+      key                 = "deployment_group"
+      value               = "${local.app_name}"
+      propogate_at_launch = true
+    }
+  ]
+}
+
+/*
+ * Create the load balancer
+ */
+module "loadbalancer" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 3.4.0"
+
+  load_balancer_name = "${local.app_name}"
+  vpc_id             = "${module.vpc.vpc_id}"
+  subnets            = ["${module.vpc.public_subnets}"]
+  security_groups    = [
+    "${module.vpc.default_security_group_id}",
+    "${module.security_group_web.this_security_group_id}",
+  ]
+
+  logging_enabled = false
+
+  http_tcp_listeners_count = 1
+  http_tcp_listeners       = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+    }
+  ]
+
+  target_groups_count = 1
+  target_groups       = [
+    {
+      name                 = "${local.app_name}-default"
+      backend_protocol     = "HTTP"
+      backend_port         = 80
+      deregistration_delay = 60
+    }
+  ]
+
+  tags = "${local.default_tags}"
+}
+
+/*
+ * Setup codedeploy 
+ */
+data "aws_iam_role" "codedeploy" {
+  name = "sunfish-codedeploy"
+}
+
+resource "aws_codedeploy_app" "this" {
+  name = "${local.app_name}"
+}
+
+resource "aws_codedeploy_deployment_group" "this" {
+  app_name              = "${aws_codedeploy_app.this.name}"
+  deployment_group_name = "${local.app_name}"
+  service_role_arn      = "${data.aws_iam_role.codedeploy.arn}"
+
+  load_balancer_info {
+    target_group_info {
+      name = "${module.loadbalancer.target_group_names[0]}"
+    }
+  }
+
+  ec2_tag_filter {
+    key   = "deployment_group"
+    type = "KEY_AND_VALUE"
+    value  = "${local.app_name}"
+  }
 }
